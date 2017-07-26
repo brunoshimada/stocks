0b9a872f4be57f9c94df5a522234197bbb3d450a
Shimada <brunoyshimada@gmail.com>
2017-07-25 23:49:39 -0300

    Versão 0.1.1 Mudanças no CHANGELOG

--- tests/stocksTest.php
+++ tests/stocksTest.php
@@ -1,8 +1,15 @@
 <?php
 require '../controller/stocks.php';
-$stock = new stocks;
-$file = $stock->getIndexes('PETR4.SA');
+require '../controller/db_connection.php';
+$stocks = new stocks;
+$db_connection = new db_connection;
+$file = $stocks->getIndexes('ITUB4.SA');
 // print("FETCH AND DECODE ALL INDEXES\n");
-// $stock->decodeAllIndexes($file);
-print("FETCH SINGLE INDEX\n");
-$stock->decodeSingleIndex($file);
+$query = $stocks->decodeAllIndexes($file);
+// print_r($query);
+// print("FETCH SINGLE INDEX\n");
+// $query = $stocks->decodeSingleIndex($file); 
+$conn = $db_connection->connect();
+// $query = 'SELECT * FROM daily_prices;';
+$res = mysqli_query($conn,$query) or trigger_error('error: '.mysqli_error($conn), E_USER_ERROR);
+// print_r($res);
